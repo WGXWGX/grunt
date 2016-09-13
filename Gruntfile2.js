1c3c0657bf4bf935617c487fd50920c59e291a85
WGXWGX <wgx15145076862@sina.com>
2016-09-13 22:48:40 +0800

    Update Gruntfile2.js

--- Gruntfile2.js
+++ Gruntfile2.js
@@ -1,11 +1,68 @@
 module.exports = function(grunt){
-  grunt.loadNpmTasks('grunt-contrib-copy');
+	  grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.initConfig({
+		copy:{
+			html:{
+				src:'index.html',
+				dest:'dest/'
+			}
+		},
+		watch:{
+			html:{
+				files:['index.html'],
+				tasks:['copy'],
+				options:{
+					livereload:true
+				}
+			},
+			css:{
+				files:['src/sass/*.scss'],
+				tasks:['sass','concat'],
+				options:{
+					livereload:true
+				}
+			}
+		},
+		connect:{
+			server1:{
+				options:{
+					port:8080,
+					base:'dest',
+					livereload:true
+				}
+			}
+		},
+		sass:{
+			css:{
+				// src:'src/sass/test1.scss',
+				// dest:'src/css/test1.css'
+				files: [{			       
+					expand: true,			        
+					cwd: 'src/sass/',			      
+					src: ['*.scss'],			       
+					dest: 'src/css/',			        
+					ext: '.css'		    
+				}]
+
+			}
+		}
+	
+	
+	});
+
+	grunt.registerTask('default',['connect','watch']);
+
+	grunt.registerTask('min',['uglify','cssmin'])
+
+
+
+
 
 
 }
