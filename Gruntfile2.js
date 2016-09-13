e205f4d0cfc9ed3d57597ecbed9f53596eeac466
WGXWGX <wgx15145076862@sina.com>
2016-09-13 22:45:59 +0800

    Create Gruntfile2.js

---
+++ Gruntfile2.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt){
+  grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+}
